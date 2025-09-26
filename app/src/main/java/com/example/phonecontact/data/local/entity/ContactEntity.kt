8446c1824b6981f4f0d37d34c70ff17733005fd3
nzlicrbc <nzlcn10@hotmail.com>
2025-09-26 15:46:08 +0300

    feat: add Room database entities for contacts and search history

---
+++ app/src/main/java/com/example/phonecontact/data/local/entity/ContactEntity.kt
@@ -0,0 +1,31 @@
+package com.example.phonecontact.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts")
+data class ContactEntity(
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String,
+
+    @ColumnInfo(name = "first_name")
+    val firstName: String,
+
+    @ColumnInfo(name = "last_name")
+    val lastName: String,
+
+    @ColumnInfo(name = "phone_number")
+    val phoneNumber: String,
+
+    @ColumnInfo(name = "profile_image_url")
+    val profileImageUrl: String? = null,
+
+    @ColumnInfo(name = "is_in_device_contacts")
+    val isInDeviceContacts: Boolean = false,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: String? = null
+)
